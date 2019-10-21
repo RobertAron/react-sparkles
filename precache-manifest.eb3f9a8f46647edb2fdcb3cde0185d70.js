5dcb9d728d99af53b90b47adffcb27af226a205b
unknown <Robert.Aron.tx@gmail.com>
2019-10-20 19:36:58 -0500

    Updates

---
+++ precache-manifest.eb3f9a8f46647edb2fdcb3cde0185d70.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a09ad82df97a02c83e7eec0a2275da2",
+    "url": "/react-sparkles/index.html"
+  },
+  {
+    "revision": "d60c4a78570d503ffcec",
+    "url": "/react-sparkles/static/css/main.21f45a90.chunk.css"
+  },
+  {
+    "revision": "5c0aa5e35f0923dec1b3",
+    "url": "/react-sparkles/static/js/2.a7131b1b.chunk.js"
+  },
+  {
+    "revision": "d60c4a78570d503ffcec",
+    "url": "/react-sparkles/static/js/main.142444e5.chunk.js"
+  },
+  {
+    "revision": "f759ff38b5b081b77ddf",
+    "url": "/react-sparkles/static/js/runtime-main.77cb99eb.js"
+  },
+  {
+    "revision": "51694392cb7135e7f69328c1a503718a",
+    "url": "/react-sparkles/static/media/sparkle1.51694392.png"
+  },
+  {
+    "revision": "7a96ebbe351f836de5b7cda1b45cad3a",
+    "url": "/react-sparkles/static/media/sparkle3.7a96ebbe.png"
+  },
+  {
+    "revision": "82f024e87ba1ccd151f12fa0eba61860",
+    "url": "/react-sparkles/static/media/sparkle4.82f024e8.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1a6eab5ac6a3191f5b2dafab121da76c.js"
+  "/react-sparkles/precache-manifest.eb3f9a8f46647edb2fdcb3cde0185d70.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-sparkles/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
