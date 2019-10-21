59ec701aa1fb5726db3fb693ae3709482bf3d588
unknown <Robert.Aron.tx@gmail.com>
2019-10-20 19:28:30 -0500

    Updates

---
+++ precache-manifest.1a6eab5ac6a3191f5b2dafab121da76c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eafd03f6dd8b832d04bdd7e64de50dd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f77518760fcaf3026ce",
+    "url": "/static/css/main.21f45a90.chunk.css"
+  },
+  {
+    "revision": "5c0aa5e35f0923dec1b3",
+    "url": "/static/js/2.a7131b1b.chunk.js"
+  },
+  {
+    "revision": "6f77518760fcaf3026ce",
+    "url": "/static/js/main.2020079a.chunk.js"
+  },
+  {
+    "revision": "5e3ecbccfd13490921a6",
+    "url": "/static/js/runtime-main.72ea2cac.js"
+  },
+  {
+    "revision": "51694392cb7135e7f69328c1a503718a",
+    "url": "/static/media/sparkle1.51694392.png"
+  },
+  {
+    "revision": "7a96ebbe351f836de5b7cda1b45cad3a",
+    "url": "/static/media/sparkle3.7a96ebbe.png"
+  },
+  {
+    "revision": "82f024e87ba1ccd151f12fa0eba61860",
+    "url": "/static/media/sparkle4.82f024e8.png"
+  }
+]);
